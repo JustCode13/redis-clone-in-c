2a47ea432b00d1b7f54953bcbf12e770b4ae56ca
codewithtea130 <tea858880@gmail.com>
2026-06-25 20:51:10 +0530

    Add explanation comments in common.h

--- include/common.h
+++ include/common.h
@@ -2,17 +2,18 @@
 #define COMMON_h
 
 
-#include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <time.h>
+#include <stdint.h> // get particular size int
+#include <stddef.h> // to use size_t
+#include <stdbool.h> // to get, use boolean, true and false
+#include <errno.h> // so we can get errors
+#include <time.h> // so we can work with time
 
-#define REDIS_MAX_KEY_SIZE 256
-#define REDIS_MAX_CLIENTS 10000
+#define REDIS_MAX_KEY_SIZE 256 // maximum size keys redis will store
+#define REDIS_MAX_CLIENTS 10000 // maxium number of clients redis will allow
 #define REDIS_LOAD_FACTOR 0.75
-#define REDIS_DEFAULT_PORT 6379
+#define REDIS_DEFAULT_PORT 6379 // default port where redis will listen
 
+// some predefine int types
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
