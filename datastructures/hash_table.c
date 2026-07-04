f2ac80563791e4711c67ebf5b76ae2e0713d47ca
codewithtea130 <tea858880@gmail.com>
2026-07-04 19:31:37 +0530

    add find_slot

--- datastructures/hash_table.c
+++ datastructures/hash_table.c
@@ -44,3 +44,62 @@ size_t probe_next(size_t index, size_t capacity){
 
     return next_index;
 }
+
+
+
+HashEntry *find_slot(HashTable *table,const char *key, u64 hash) {
+    if (table == NULL || key == NULL) {
+
+        return NULL;
+    }
+
+    if (table -> entries == NULL || table -> capacity == 0) {
+
+        return NULL;
+    }
+
+    size_t index = hash % table -> capacity;
+    size_t visited = 0;
+    HashEntry *first_tombstone = NULL;
+
+    while (visited < table -> capacity){
+
+        HashEntry *const entry = &table -> entries[index];
+
+        if (entry -> state == EMPTY) {
+
+            if (first_tombstone != NULL) {
+                return first_tombstone;
+            }
+
+            return entry;
+        }
+
+        if (entry -> state == OCCUPIED){
+
+            if (hash == entry -> hash){
+
+                if (strcmp(entry -> key, key) == 0) {
+
+                    return entry;
+                }
+            }
+        }
+
+        if (entry -> state == TOMBSTONE && first_tombstone == NULL){
+                first_tombstone = entry;
+        }
+
+        index = probe_next(index,table -> capacity);
+        
+        visited ++;
+    }
+
+    if (first_tombstone != NULL){
+
+        return first_tombstone;
+    }
+
+    return NULL;
+}
+
