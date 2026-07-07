0db96385696b1437a9662fdb9ef57c5f030a633c
codewithtea130 <tea858880@gmail.com>
2026-07-07 18:08:24 +0530

    add align_pointer

---
+++ memory/memory_utils.c
@@ -0,0 +1,21 @@
+#include "../include/memory.h"
+
+static MemoryStats memory_stats;
+
+void *align_pointer(void *ptr, size_t alignment) {
+    if (ptr == NULL || alignment == 0){
+        return NULL;
+    }
+
+    if ((alignment & (alignment -1)) != 0){
+        return NULL;
+    }
+
+    uptr_t int_ptr = (uptr_t)ptr;
+
+    uptr_t aligned = (int_ptr + alignment - 1) & ~(alignment - 1);
+
+    void *aligned_ptr = (void *)aligned;
+
+    return aligned_ptr;
+}
