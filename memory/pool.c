17cc52d962e198fea4f329cd9aa52ab88a22145b
codewithtea130 <tea858880@gmail.com>
2026-07-07 12:55:10 +0530

    add pool_alloc

--- memory/pool.c
+++ memory/pool.c
@@ -21,15 +21,13 @@ PoolAllocator *pool_create(size_t object_size, size_t object_count, size_t align
     }
 
     if (object_size > SIZE_MAX - (alignment - 1)) {
-        free(pool);
-        return NULL;
+        goto cleanup;
     }
         
     size_t aligned_object_size = (object_size + alignment - 1) & ~(alignment - 1);
 
     if (object_count > SIZE_MAX / aligned_object_size){
-        free(pool);
-        return NULL;
+        goto cleanup;
     }
 
     size_t total_size = aligned_object_size * object_count;
@@ -37,26 +35,24 @@ PoolAllocator *pool_create(size_t object_size, size_t object_count, size_t align
     void *memory = malloc(total_size);
 
     if (memory == NULL){
-        free(pool);
-        return NULL;
+        goto cleanup;
     }
 
-    u8 *base = memory;
+    u8 *base = (u8 *)memory;
 
     for (size_t i = 0; i < object_count; i++){
 
-        FreeBlock *block = (base + i * aligned_object_size);
+        FreeBlock *block = (FreeBlock *)(base + i * aligned_object_size);
 
         block->size = aligned_object_size;
 
         if (i == object_count -1){
             block -> next = NULL;
         } else {
-            block -> next = (FreeBlock *)(base + (i + 1) * aligned_object_size);
+            u8 *next_address = base + ( i + 1) * aligned_object_size;
+            block -> next = (FreeBlock *)next_address;
         }
     }
-
-    FreeBlock *free_block = malloc(object_count * sizeof(aligned_object_size));
 
     pool -> memory = memory;
     pool -> free_list = (FreeBlock *)memory;
@@ -65,5 +61,27 @@ PoolAllocator *pool_create(size_t object_size, size_t object_count, size_t align
     pool -> alignment = alignment;
     
     return pool;
+
+
+    cleanup:
+    free(pool);
+    return NULL;
 }
 
+
+void *pool_alloc(PoolAllocator *pool){
+    if (pool == NULL){
+        fprintf(stderr, "No free client objects available\n");
+        return NULL;    
+    }
+
+    if (pool -> free_list == NULL) {
+        return NULL;
+    }
+
+    FreeBlock *block = pool -> free_list;
+
+    pool -> free_list = block -> next;
+
+    return (void *)block;
+}
