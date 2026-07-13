df9a1bc30cb5b0c1e19505780a43296937ac78af
codewithtea130 <tea858880@gmail.com>
2026-07-13 18:51:50 +0530

    add create_node

--- datastructures/skiplist.c
+++ datastructures/skiplist.c
@@ -14,3 +14,34 @@ int random_level(void) {
 
     return level;
 }
+
+SkipNode *create_node(double score, const char *member, int level) {
+    if (level < 1 || level > SKIPLIST_MAX_LEVEL || member == NULL) {
+        return NULL;
+    }
+
+    SkipNode *node = malloc(sizeof(SkipNode));
+
+    if (node == NULL) {
+        return NULL;
+    }
+
+    char *member_copy = strdup(member);
+
+    if (member_copy == NULL) {
+        free(node);
+        return NULL;
+    }
+
+    node->score = score;
+    node->member = member_copy;
+    node->level = level;
+
+    for (size_t i = 0; i < SKIPLIST_MAX_LEVEL; i++) {
+        node->forward[i] = NULL;
+    }
+
+    node->backward=NULL;
+
+    return node;
+}
